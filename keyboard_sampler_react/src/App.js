15a8a746555aa938a08f1b0d79e7b762a5262a1e
unknown <el.zwierzakko@gmail.com>
2020-12-23 20:49:02 +0100

    basic code updates

--- keyboard_sampler_react/src/App.js
+++ keyboard_sampler_react/src/App.js
@@ -1,4 +1,131 @@
+import React from  'react';
 import './App.css';
+
+const URL = "https://s3.amazonaws.com/freecodecamp/drums/";
+const SOUNDFILE_NAMES_AND_KEYS = {
+  Q : "Heater-1",
+  W : "Kick_n_Hat",
+  E : "Chord_1",
+  A : "Heater-6",
+  S : "punchy_kick_1", 
+  D : "Brk_Snr",
+  Z : "Dry_Ohh",
+  X : "Heater-3",
+  C : "Dsc_Oh"
+}
+
+// This is to pass the last test.
+const KEYCODES = {
+  81 : 'Q',
+  87 : 'W',
+  69 : 'E',
+  65 : 'A',
+  83 : 'S',
+  68 : 'D',
+  90 : 'Z',
+  88 : 'X',
+  67 : 'C'
+};
+
+class SamplePlayer extends React.Component {
+  constructor(props){
+    super();
+    this.state = { 
+      currentSoundId: ''
+    };
+    this.handleClickPlay = this.handleClickPlay.bind(this);
+    this.handleKeyPress = this.handleKeyPress.bind(this);
+    this.playSound = this.playSound.bind(this);
+  }
+  
+ componentDidMount() {
+   document.addEventListener("keydown", this.handleKeyPress);
+  }
+  
+  componentWillUnmount() {
+document.removeEventListener("keydown", this.handleKeyPress);
+  }
+  
+  playSound(){
+    document.getElementById(this.state.currentSoundId).play();
+  }
+
+/* Play audio with javascript
+http://stackoverflow.com/questions/9419263/ddg#18628124
+
+The Joy of HTML5 Audio: Tips & Tricks for Easy Sound Embedding
+https://www.elated.com/articles/html5-audio/
+*/
+  
+  
+  /* To do:
+  It would be nice if when the user presses a key from the drum pad, the "button" reacts the same way as when the user hovers them with the mouse.
+  */
+  handleKeyPress(event){
+    
+    // const key = event.key.toUpperCase();
+    
+    const key = KEYCODES[event.keyCode]; // this is to pass the last test.
+    /* 
+    Here: https://developer.mozilla.org/en-US/docs/Web/API/KeyboardEvent/keyCode 
+    says that *keyCode* is deprecated and to avoid using it. 
+    And here: https://developer.mozilla.org/en-US/docs/Web/API/KeyboardEvent
+    it is recommended to use *key* instead.
+    */
+    
+    const isValidKey = Object.keys(SOUNDFILE_NAMES_AND_KEYS).includes(key);
+    this.setState( { currentSoundId: (isValidKey)? key : '' } );
+  }
+      
+  handleClickPlay(event){
+    this.setState( { currentSoundId: event.target.children[0].id } );
+  }
+  
+  render() {
+    let message = 'No sound';
+    if (this.state.currentSoundId) {
+      message = `Sound: ${SOUNDFILE_NAMES_AND_KEYS[this.state.currentSoundId]}`;
+      this.playSound();  
+    }
+    
+    return (
+      <div id="sample-listing">
+        <Display currentSoundText={message}/>
+        <div id="sample-icons">
+          {
+            Object.keys(SOUNDFILE_NAMES_AND_KEYS).map((key) => {
+              return (
+                <SamplerMachine
+                  name={key} 
+                  soundFile={URL + SOUNDFILE_NAMES_AND_KEYS[key] + ".mp3"}
+                  clickplayer={this.handleClickPlay}
+                  key={key}
+                />
+              )
+            })
+          }
+        </div>
+      </div>
+    );
+  }
+}
+
+function Display(props){
+    return (
+    <div id="display">
+      {props.currentSoundText}
+    </div>
+  )
+}
+
+function SamplerMachine(props) {
+  return (
+    <li className="sample-icon" id={props.soundFile} onClick={props.clickplayer}>
+      {props.name}
+      <audio className="clip" id={props.name} src={props.soundFile} type="audio/mp3" ></audio>
+    </li>
+  )
+}
 
 
 function App() {
@@ -15,65 +142,37 @@ function App() {
             <div class="keyboard-leayout"></div>
 
             <ul class="sample-listing">
-            <li class="sample1">
-                <p>"L SHIFT"</p>
-                <p>SAMPLE 1</p>
-
-                <audio id="sample1" controls style={{display:"none"}}>
-                <source src="res/sample1.wav" type="audio/mpeg"></source>
-                </audio>
-                
-                <audio id="sample3" style={{display:"none"}}>
-                <source src="res/sample3.wav" type="audio/mpeg"></source>
-                </audio>
-
-                <audio id="sample5" controls style={{display:"none"}}>
-                <source src="res/sample5.wav" type="audio/mpeg"></source>
-                </audio>
-
-                <audio id="sample7" controls style={{display:"none"}}>
-                <source src="res/sample7.wav" type="audio/mpeg"></source>
-                </audio>
-
-            </li>
             
-            <li class="change-sample-group">
-                <p>"Y"</p>
-                <p>NEXT PRESET</p>
+            <li class="sampler-main">
+              
+                <SamplePlayer/>
 
-                <div class="change-sample-group"></div>
             </li>
 
-            <li class="sample2">
-                <p>"R SHIFT"</p>
-                <p>SAMPLE 2</p>
-
-                <audio id="sample2" controls style={{display:"none"}}>
-                <source src="res/sample2.wav" type="audio/mpeg"></source>
-
-                <audio id="sample4" controls style={{display:"none"}}>
-                <source src="res/sample4.wav" type="audio/mpeg"></source>
-                </audio>
-
-                <audio id="sample6" controls style={{display:"none"}}>
-                <source src="res/sample6.wav" type="audio/mpeg"></source>
-                </audio>
-
-                <audio id="sample8" controls style={{display:"none"}}>
-                <source src="res/sample8.wav" type="audio/mpeg"></source>
-                </audio>
-              </audio>
-            </li>
             </ul>
         </div>
-        <div class="selected-preset-color">
-            <h1>PRESS "Y" TO FOR NEXT PRESET</h1>
-            <p class="info">There are 8 sample presets in total. Each has it's own color.</p>
-            <p class="info">Each sample preset can contain 2 samples. Use the R and L SHIFT keys to play the samples. The "Y" key will change sample preset.</p>
-            <div id="color" class="color">
-            PRESET 1        
+        <div class="details-container">
+            <h1>PRESS THE MAPPED KEYS TO PLAY SAMPLES</h1>
+            <p class="info">9 samples total can be used.</p>
+            <p class="info">The sampler will display the sample name once a mapped key is pressed. Otherwise "No Sample" message will be displayed.</p>
+            <div id="color" class="color">    
         </div>
+        
     </div>
+
+    <div class="youtube-info-container">
+    <div class="youtube">
+    </div>
+
+    <div class="app-info">
+    </div>
+
+
+    <div class="change-preset">
+    </div>
+
+    </div>
+    
     </div>
     </body>
 
